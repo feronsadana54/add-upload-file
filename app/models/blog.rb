0a62b1028d7227aa126821772a38878fa34bab94
Feron Sadana <feronsadana16@gmail.com>
2022-05-12 20:23:52 +0700

    add upload image

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,25 @@
 class Blog < ApplicationRecord
+    has_one_attached :image
+
+    validates :title, presence: true
+    validates :body, presence: true 
+    validate :validate_image
+
+
+
+    private def validate_image 
+        if image.attached? && !image.content_type.in?(%('image/jpeg image/png'))
+            errors.add(:image, 'must be a JPEG or PNG')
+        elsif image.attached? == false 
+                errors.add(:image, 'must be attached')
+        end
+    end
+
+    private def validate_file 
+        if file.attached? && !file.content_type.in?(%('application/pdf application/doc '))
+            errors.add(:file, 'must be a PDF or DOC')
+        elsif file.attached? == false 
+            errors.add(:file, 'must be attached')
+        end
+    end
 end
